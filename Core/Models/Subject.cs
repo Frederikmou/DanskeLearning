d407226aec1f12a7eed1559417908e917e0bdbf7
MagnusGregers <eaa24mgmw@students.eaaa.dk>
2025-11-27 12:34:26 +0100

    Added mock service

--- Core/Models/Subject.cs
+++ Core/Models/Subject.cs
@@ -4,5 +4,5 @@ public class Subject
 {
     public int subjectId { get; set; }
     public string title { get; set; }
-    public string category  { get; set; }
+    
 }
--- DanskeLearning/Services/DashboardService/DashboardService.cs
+++ DanskeLearning/Services/DashboardService/DashboardService.cs
@@ -1,6 +1,20 @@
+using Core.Models;
+
 namespace DanskeLearning.Services.DashboardService;
 
-public class DashboardService
+public class DashboardService : IDashboardSerivce
 {
+    public async Task<List<Subject>> GetSubjectsAsync()
+    {
+        await Task.Delay(1);
+
+        return new List<Subject>
+        {
+            new Subject { subjectId = 1, title = "Accounts" },
+            new Subject { subjectId = 2, title = "Cards" },
+            new Subject { subjectId = 3, title = "AML" },
+        };
+    }
+    
     
 }
