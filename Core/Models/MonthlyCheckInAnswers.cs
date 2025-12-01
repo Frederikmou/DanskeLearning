1fdd0f581f9bd8052e43985aec3361d3854d78ce
signe92 <eaa24sieb@students.eaaa.dk>
2025-12-01 10:27:57 +0100

    Tabeller for mygrowth

--- Core/Models/MonthlyCheckInAnswers.cs
+++ Core/Models/MonthlyCheckInAnswers.cs
@@ -3,7 +3,10 @@
 public class MonthlyCheckInAnswers
 {
     public int answerId { get; set; }
-    public int checkinId { get; set; }
+    public int checkInId { get; set; }
+    public int userId { get; set; }
     public int questionId { get; set; }
     public string answerText { get; set; }
+    public DateTime answerDate { get; set; }
+    
 }
